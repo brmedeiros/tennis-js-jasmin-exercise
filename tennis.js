1a97dfad86dfa0b36661cf69ddb8f75cb56cad18
brmedeiros <br.medeiros@gmail.com>
2017-09-07 16:05:51 -0300

    adds the getGameScore() and checkWinner() methods
    - adds jasmine tests for both methods
    - modifies the score() method to account for player advantages in
      a deuce

--- tennis.js
+++ tennis.js
@@ -13,6 +13,8 @@ function Game(player1, player2) {
 
     this.players = {[player1]: gameInit(),
 		    [player2]: gameInit()};
+    this.p1name = Object.keys(this.players)[0];
+    this.p2name = Object.keys(this.players)[1];
 }
 
 Game.prototype.getPlayers = function() {
@@ -26,9 +28,32 @@ Game.prototype.getPlayerScore = function(player){
 Game.prototype.score = function(player){
     if(this.players[player]['score'] < 30){
 	this.players[player]['score'] += 15;
-    }
-    else if (this.players[player]['score'] != 40){
+    } else if (this.players[player]['score'] < 40) {
 	this.players[player]['score'] = 40;
+    } else if((this.players[player]['score'] == 40) //this will need to be fixed
+	      && this.players[player]['adv'] == false){
+	this.players[player]['adv'] = true;
+    }
+};
+
+Game.prototype.getGameScore = function(){
+    if(this.players[this.p1name]['score'] < 40 || this.players[this.p2name]['score'] < 40){
+	return this.p1name + ' ' + this.players[this.p1name]['score'] + ' vs ' + this.p2name + ' ' + this.players[this.p2name]['score'];
+    } else if(this.players[this.p1name]['adv'] == false && this.players[this.p2name]['adv'] == false){
+	return 'Deuce';
+    } else{
+	return 'Deuce: Advantage ' + this.p1name; //this will need to be fixed
+    }
+    
+};
+
+Game.prototype.checkWinner = function(){
+    if(this.players[this.p1name]['score'] == 40 && this.players[this.p2name]['score'] < 40
+       && this.players[this.p1name]['adv'] == true){
+	return this.p1name;
+    } else if(this.players[this.p2name]['score'] == 40 && this.players[this.p1name]['score'] < 40
+	      && this.players[this.p2name]['adv'] == true){
+	return this.p2name;
     }
 };
 
--- tennisTest.js
+++ tennisTest.js
@@ -45,7 +45,7 @@ describe("The Game", function() {
 	expect(game.getPlayerScore('Rakan')['adv']).toBe(false);
     });
 
-    it("should make a player score 40 when they score a second time", function(){
+    it("should make a player score 40 when they score a third time", function(){
 	var game =  new Game('Gnar', 'Rakan');
 	game.score('Gnar');
 	game.score('Gnar');
@@ -55,5 +55,78 @@ describe("The Game", function() {
 	expect(game.getPlayerScore('Rakan')['score']).toBe(15);
 	expect(game.getPlayerScore('Gnar')['adv']).toBe(false);
 	expect(game.getPlayerScore('Rakan')['adv']).toBe(false);
+	expect(game.checkWinner()).toBe(undefined);
+    });
+
+    it("should have a winner when a player has 40 points, scores and the other player has 30 points or less", function(){
+	var game1 =  new Game('Gnar', 'Rakan');
+	game1.score('Gnar');
+	game1.score('Gnar');
+	game1.score('Gnar');
+	game1.score('Gnar');
+	expect(game1.getPlayerScore('Gnar')['score']).toBe(40);
+	expect(game1.getPlayerScore('Rakan')['score']).toBe(0);
+	expect(game1.checkWinner()).toBe('Gnar');
+
+	var game2 =  new Game('Gnar', 'Rakan');
+	game2.score('Rakan');
+	game2.score('Rakan');
+	game2.score('Gnar');
+	game2.score('Gnar');
+	game2.score('Rakan');
+	game2.score('Rakan');
+	expect(game2.getPlayerScore('Gnar')['score']).toBe(30);
+	expect(game2.getPlayerScore('Rakan')['score']).toBe(40);
+	expect(game2.checkWinner()).toBe('Rakan');
+    });
+
+    it("should give a player advantage when they score and both players have 40 points and no advantages", function(){
+	var game =  new Game('Katarina', 'Garen');
+	game.score('Katarina');
+	console.log(game.getGameScore());
+	game.score('Katarina');
+	console.log(game.getGameScore());
+	game.score('Garen');
+	console.log(game.getGameScore());
+	game.score('Garen');
+	console.log(game.getGameScore());
+	game.score('Katarina');
+	console.log(game.getGameScore());
+	game.score('Garen');
+	console.log(game.getGameScore());
+	game.score('Katarina');
+	console.log(game.getGameScore());
+	expect(game.getPlayerScore('Katarina')['score']).toBe(40);
+	expect(game.getPlayerScore('Garen')['score']).toBe(40);
+	expect(game.getPlayerScore('Katarina')['adv']).toBe(true);
+	expect(game.getPlayerScore('Garen')['adv']).toBe(false);
+    });
+
+    it("should tell the correct score for both players at anytime", function(){
+	var game1 =  new Game('Sona', 'Lux');
+	game1.score('Lux');
+	game1.score('Sona');
+	game1.score('Sona');
+	game1.score('Lux');
+	expect(game1.getGameScore()).toBe('Sona' + ' ' + '30' +' vs ' + 'Lux' + ' ' + '30');
+	
+	var game2 =  new Game('Varus', 'Tristana');
+	game2.score('Tristana');
+	game2.score('Tristana');
+	game2.score('Tristana');
+	expect(game2.getGameScore()).toBe('Varus' + ' ' + '0' +' vs ' + 'Tristana' + ' ' + '40');
+    
+	var game3 =  new Game('Varus', 'Tristana');
+	game3.score('Varus'); //15-0
+	game3.score('Tristana'); //15-15
+	game3.score('Tristana'); //15-30
+	game3.score('Varus'); //30-30
+	game3.score('Varus'); //40-30
+	game3.score('Tristana'); //deuce
+	expect(game3.getPlayerScore('Tristana')['adv']).toBe(false);
+	expect(game3.getPlayerScore('Varus')['adv']).toBe(false);
+	expect(game3.getGameScore()).toBe('Deuce');
+	game3.score('Varus'); // deuce adv Varus
+	expect(game3.getGameScore()).toBe('Deuce: Advantage ' + 'Varus');
     });
 });
