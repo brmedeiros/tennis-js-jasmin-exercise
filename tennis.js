a10e6c71b24c9ff167ee8bdd5ab7bc11860ce911
brmedeiros <br.medeiros@gmail.com>
2017-09-07 00:52:45 -0300

    adds a .gitignore file adds the Game constructor
    - adds the methods getPlayers(), getScore() and score()
    - adds the respective jasmine tests

--- tennis.js
+++ tennis.js
@@ -1,4 +1,35 @@
 function HelloWorld() {
     return 'Hello Tennis World';
 }
+
 document.getElementById("id1").innerHTML = HelloWorld();
+
+// Game constructor start
+
+function Game(player1, player2) {
+    function gameInit(){
+	return {'score': 0, 'adv': false};
+    };
+
+    this.players = {[player1]: gameInit(),
+		    [player2]: gameInit()};
+}
+
+Game.prototype.getPlayers = function() {
+    return this.players;
+};
+
+Game.prototype.getScore = function(player){
+    return this.players[player];
+};
+
+Game.prototype.score = function(player){
+    if(this.players[player]['score'] < 30){
+	this.players[player]['score'] += 15;
+    }
+    else if (this.players[player]['score'] != 40){
+	this.players[player]['score'] = 40;
+    }
+};
+
+//Game constructor end
--- tennisTest.js
+++ tennisTest.js
@@ -1,7 +1,59 @@
-describe("HelloWorld()", function(){ 
+// describe("HelloWorld()", function() { 
 
-   it("should Return Hello Tennis world",function(){ 
-      expect(HelloWorld()).toBe('Hello Tennis World');
-   }); 
+//    it("should Return Hello Tennis world", function() { 
+//       expect(HelloWorld()).toBe('Hello Tennis World');
+//    });
 
+// });
+
+describe("The Game", function() {
+
+    it("should have two players", function() {
+	var game =  new Game('player1', 'player2');
+	expect(Object.keys(game.getPlayers()).length).toBe(2);
+    });
+
+    it("should start with zero score for both players", function(){
+	var game =  new Game('Ahri', 'Nunu');
+	expect(game.getScore('Ahri')['score']).toBe(0);
+	expect(game.getScore('Nunu')['score']).toBe(0);
+    });
+
+    it("should start with no advantage to either player", function(){
+	var game =  new Game('Shyvana', 'Ezreal');
+	expect(game.getScore('Shyvana')['adv']).toBe(false);
+	expect(game.getScore('Ezreal')['adv']).toBe(false);
+    });
+
+    it("should make a player score 15 when a player score for the first time", function(){
+	var game =  new Game('Gnar', 'Rakan');
+	game.score('Gnar');
+	expect(game.getScore('Gnar')['score']).toBe(15);
+	expect(game.getScore('Rakan')['score']).toBe(0);
+	expect(game.getScore('Gnar')['adv']).toBe(false);
+	expect(game.getScore('Rakan')['adv']).toBe(false);
+    });
+
+    it("should make a player score 30 when they score a second time", function(){
+	var game =  new Game('Gnar', 'Rakan');
+	game.score('Gnar');
+	game.score('Rakan');
+	game.score('Rakan');
+	expect(game.getScore('Gnar')['score']).toBe(15);
+	expect(game.getScore('Rakan')['score']).toBe(30);
+	expect(game.getScore('Gnar')['adv']).toBe(false);
+	expect(game.getScore('Rakan')['adv']).toBe(false);
+    });
+
+    it("should make a player score 40 when they score a second time", function(){
+	var game =  new Game('Gnar', 'Rakan');
+	game.score('Gnar');
+	game.score('Gnar');
+	game.score('Gnar');
+	game.score('Rakan');
+	expect(game.getScore('Gnar')['score']).toBe(40);
+	expect(game.getScore('Rakan')['score']).toBe(15);
+	expect(game.getScore('Gnar')['adv']).toBe(false);
+	expect(game.getScore('Rakan')['adv']).toBe(false);
+    });
 });
