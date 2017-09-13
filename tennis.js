9cd76eb83d419f985d9d44adb5dec5719cc10e47
brmedeiros <br.medeiros@gmail.com>
2017-09-13 14:57:07 -0300

    refactors the methods score() and getWinner()

--- tennis.js
+++ tennis.js
@@ -14,11 +14,12 @@ function Game(player1, player2){
 	return {'score': 0, 'adv': false, 'winner': false};
     };
 
-    this.players = {[player1]: gameInit(),
-		    [player2]: gameInit()};
+    this.players = {};
+    this.players[player1] = gameInit();
+    this.players[player2] = gameInit();
     // key name strings
-    this.p1name = Object.keys(this.players)[0];
-    this.p2name = Object.keys(this.players)[1];
+    this.p1name = player1;
+    this.p2name = player2;
 }
 
 Game.prototype.getPlayers = function(){
@@ -37,27 +38,24 @@ Game.prototype.score = function(player){
 	// if the player has 30 points, make their score 40
 	this.players[player]['score'] = 40;
     } else if(this.players[player]['score'] == 40){
-	if(this.players[this.p1name]['adv'] == false && this.players[this.p2name]['adv'] == false){
-	    // if the player has 40 points, scores and no player has
-	    // the advantage, give them the advantage
-	    this.players[player]['adv'] = true;
-	} else if(this.players[this.p1name]['adv'] == false && this.players[this.p2name]['adv'] == true
-		  && player == this.p1name){
-	    // if the first player scores and the second player has
-	    // the advantage, remove the advantage
-	    this.players[this.p2name]['adv'] = false;
-	} else if(this.players[this.p1name]['adv'] == true && this.players[this.p2name]['adv'] == false
-		  && player == this.p2name){
-	    // if the second player scores and the first player has
-	    // the advantage, remove the advantage
-	    this.players[this.p1name]['adv'] = false;
-	} else if(this.players[player]['adv'] == true){
+	// if the player has 40
+	other_player = player == this.p1name ? this.p2name : this.p1name;
+	if(this.players[player]['adv'] || this.players[other_player]['score'] < 40) {
+	    // and has the advantage or the other player has less than 40, they are the winner
 	    this.players[player]['winner'] = true;
+	} else if (this.players[other_player]['adv']) {
+	    // does not have the advantage and the other player has it
+	    // remove the advantage from the other player
+	    this.players[other_player]['adv'] = false;
+	} else {
+	    // no player has the advantage
+	    // give the advantage to the player that scored
+	    this.players[player]['adv'] = true;
 	}
     }
 };
 
-Game.prototype.getGameScore = function(){
+Game.prototype.getGameScore = function() {
     if(this.players[this.p1name]['score'] < 40 || this.players[this.p2name]['score'] < 40){
 	// if at least 1 player has less than 40 points
 	return this.p1name + ' ' + this.players[this.p1name]['score'] + ' vs ' + this.p2name + ' ' + this.players[this.p2name]['score'];
@@ -65,32 +63,19 @@ Game.prototype.getGameScore = function(){
 	// if both players have 40 points and no advantages
 	return 'Deuce';
     } else if(this.players[this.p1name]['adv'] == true){
-	//ternary operators?
 	// if both players have 40 points and one of them has the advantage
 	return 'Deuce: Advantage ' + this.p1name;
     } else {
 	// if both players have 40 points and the other one has the advantage
 	return 'Deuce: Advantage ' + this.p2name;
     }
-    
 };
 
 Game.prototype.checkWinner = function(){
-    // should probably use ternary operators
-    if(this.players[this.p1name]['score'] == 40 && this.players[this.p2name]['score'] < 40
-       && this.players[this.p1name]['adv'] == true){
-	// if the first player has 40 points and the advantage and the second player
-	// has less then 40 points, the first player is the winner
-	return this.p1name;
-    } else if(this.players[this.p2name]['score'] == 40 && this.players[this.p1name]['score'] < 40
-	      && this.players[this.p2name]['adv'] == true){
-	// same thing for the second player
-	return this.p2name;
-    } else if(this.players[this.p1name]['score'] == 40 && this.players[this.p2name]['score'] == 40){
-	if(this.players[this.p1name]['winner'] == true){
-	    return this.p1name;
-	} else if(this.players[this.p2name]['winner'] == true){
-	    return this.p2name;
+    var player;
+    for(player of Object.keys(this.players)) {
+	if( this.players[player]['winner']){
+	    return player;
 	}
     }
 };
